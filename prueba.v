9dbc2df1b945c1fbdd9d836198bd4d2d3462a69d
diana_maldonado <dnmaldonador@unal.edu.co>
2022-04-30 09:00:41 -0500

    Negativo resta

--- prueba.v
+++ prueba.v
@@ -1,17 +1,22 @@
 module prueba  (
-    input  [15,0] a,
-    input  [15,0] b,
-    output [15,0] x_out,
+    input  [15:0] a,
+    input  [15:0] b,
+    output [15:0] out_and,
+    output [15:0] out_or,
+    output [15:0] out_nand,
+    output [15:0] out_nor,
+    output [15:0] out_xor,
+    output [15:0] out_xnor
 );
 
 
 //bitwise operation
-assign x_out = a && b;  //And
-assign x_out = a || b;  //Or
-assign x_out = ~(a && b);  //Nand
-assign x_out = ~(a || b);  //Nor
-assign x_out = ~a&&b || a&&~b; //Xor
-assign x_out = ~(~a&&b || a&&~b); //Xnor 
+assign out_and = a && b;  //And
+assign out_or = a || b;  //Or
+assign out_nand = ~(a && b);  //Nand
+assign out_nor = ~(a || b);  //Nor
+assign out_xor = ~a&&b || a&&~b; //Xor 
+assign out_xnor = ~(~a&&b || a&&~b); //Xnor 
 
 
 endmodule
--- prueba_TB.v
+++ prueba_TB.v
@@ -6,17 +6,17 @@ module prueba_TB ();
     reg input2_prueba_TB;
 
     prueba uut (
-        .input1_prueba(input1_prueba_TB),
-        .input2_prueba(input2_prueba_TB)
+        .a(input1_prueba_TB),
+        .b(input2_prueba_TB)
     );
 
     initial begin
-        input1_prueba_TB = 1'b0;
-        input2_prueba_TB = 1'b0; 
+        input1_prueba_TB = 16'b0000;
+        input2_prueba_TB = 16'b0; 
         #2;
-        input1_prueba_TB = 1'b1;
+        input1_prueba_TB = 16'b1;
         #2;
-        input2_prueba_TB = 1'b1;
+        input2_prueba_TB = 16'b1;
     end
 
     initial begin: TEST_CASE
